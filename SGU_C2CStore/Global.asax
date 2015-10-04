b344aa012426fbda08a472370d470f584c4476a8
Toan Truong <truongtoansgu@gmail.com>
2015-10-04 16:45:29 +0700

    init project

---
+++ SGU_C2CStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SGU_C2CStore.MvcApplication" Language="C#" %>
---
+++ SGU_C2CStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SGU_C2CStore.Startup))]
+namespace SGU_C2CStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
