e20a2f8206fc1627988a78b5bfe6921263dd6f41
Toan Truong <ht.hitech1603@gmail.com>
2015-11-23 21:28:26 +0700

    change seed method

--- SGU_C2CStore.Services/Migrations/Configuration.cs
+++ SGU_C2CStore.Services/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
+﻿using SGU_C2CStore.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
 namespace SGU_C2CStore.Services.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<SGU_C2CStore.Services.DAL.SGUStoreServiceContext>
     {
@@ -14,18 +17,31 @@ namespace SGU_C2CStore.Services.Migrations
 
         protected override void Seed(SGU_C2CStore.Services.DAL.SGUStoreServiceContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var Categories = new List<Category>()
+            {
+                new Category() { Id = 1, Name = "Phụ kiện" },
+                new Category() { Id = 2, Name = "Điện thoại" },
+                new Category() { Id = 3, Name = "Máy tính, laptop" },
+                new Category() { Id = 4, Name = "Khác" }
+            };
+            Categories.ForEach(e => context.Categories.Add(e));
+            context.SaveChanges();
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var Products = new List<Product>()
+            {
+                new Product() {Name = "Product 1", Description = "A product 1", Price=10, CategoryId=1},
+                new Product() {Name = "Product 2", Description = "A product 2", Price=11, CategoryId=1},
+                new Product() {Name = "Product 3", Description = "A product 3", Price=12, CategoryId=2},
+                new Product() {Name = "Product 4", Description = "A product 4", Price=13, CategoryId=1},
+                new Product() {Name = "Product 5", Description = "A product 5", Price=14, CategoryId=1},
+                new Product() {Name = "Product 6", Description = "A product 6", Price=15, CategoryId=4},
+                new Product() {Name = "Product 7", Description = "A product 7", Price=16, CategoryId=3},
+                new Product() {Name = "Product 8", Description = "A product 8", Price=17, CategoryId=3},
+                new Product() {Name = "Product 9", Description = "A product 9", Price=18, CategoryId=1},
+                new Product() {Name = "Product 10", Description = "A product 10", Price=19, CategoryId=4},
+            };
+            Products.ForEach(e => context.Products.Add(e));
+            context.SaveChanges();
         }
     }
 }
